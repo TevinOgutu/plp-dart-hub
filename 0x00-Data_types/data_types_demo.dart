e6a0f2cd1c28005e5ff29d6bfe92fd3a8ad1cfcc
TevinOgutu <tevinodhis@gmail.com>
2024-03-02 23:14:01 +0300

    Fluttering melodies

---
+++ 0x00-Data_types/data_types_demo.dart
@@ -0,0 +1,22 @@
+// This program shows examples of different data types in Dart
+void main() {
+// Example of a int data type
+  int number = 24;
+  print(number);
+
+// Example of double data type
+  double decimal = 0.7;
+  print(decimal);
+
+// Example of a String data type
+  String name = "El jefe";
+  print(name);
+
+// Example of a List data type
+  List<String> fruits = ["Apple", "Banana", "Orange"];
+  print(fruits);
+
+// Example of a Map data type
+  Map<String, dynamic> player = {"name": "Haaland", "goals": 51};
+  print(player);
+}
